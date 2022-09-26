84f1c70c16b24fb1634a6163308e333258a175f5
ELerma <70728294+elerma12@users.noreply.github.com>
2022-09-26 17:33:10 -0500

    Update bitmap_message.py

--- bitmap_message.py
+++ bitmap_message.py
@@ -32,17 +32,17 @@ bitmap = """
 print(' Bitmap Message, by Al Sweigart al@inventwithphython.com') 
 print(' Enter the message to display with the bitmap.')
 message = input( '> ')
-if message== ' ':
+if message == '':
     sys.exit()
 #loop over each line in the bitmap:
 for line in bitmap.splitlines():
-     #loop over each character in the line: 
-     for i, bit in enumerate(line):
-          if bit == ' ':
-               # Print an empty space since there's a space in the bitmap:
-               print(' ',end= ' ')
-          else:
-               # print a character from the message:
-               print(message[i%len(message)], end= ' ')
-     print() # print a new line 
+    #loop over each character in the line: 
+    for i, bit in enumerate(line):
+         if bit == ' ':
+            # Print an empty space since there's a space in the bitmap:
+            print(' ', end= ' ')
+         else:
+             # print a character from the message:
+             print(message[i % len(message)], end= ' ')
+    print() # print a new line 
   
