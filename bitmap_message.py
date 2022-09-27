4489bb0fc008113491d743aa64ea3e4a6c1b072a
ELerma <70728294+elerma12@users.noreply.github.com>
2022-09-27 07:40:29 -0500

    Update bitmap_message.py

--- bitmap_message.py
+++ bitmap_message.py
@@ -39,8 +39,8 @@ for line in bitmap.splitlines():
     #loop over each character in the line: 
     for i, bit in enumerate(line):
          if bit == ' ':
-            # Print an empty space since there's a space in the bitmap:
-            print(' ', end='')
+             # Print an empty space since there's a space in the bitmap:
+             print(' ', end='')
          else:
              # print a character from the message:
              print(message[i % len(message)], end='')
